12ef4c51cb99f30f5f662432a06e86275a80e067
Scilentvishal <vishalrahangdale14@gmail.com>
2024-04-25 22:03:10 +0530

    authentication completed

--- Routes/routes.js
+++ Routes/routes.js
@@ -1,13 +1,15 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const router = require("express").Router();
 
 router.post("/register", async (req, res) => {
-    
+  // console.log(`resss: ${req.body}`)
   try {
-    const salt = await bcrypt.getSalt(10)
-    const { name, email,password, hobbies, gender, mobile, profileImage } = req.body;
-const hashedPassword = await bcrypt.hash(password, salt)
+    const salt = await bcrypt.genSalt(10);
+    const { name, email, password, hobbies, gender, mobile, profileImage } =
+      req.body;
+    const hashedPassword = await bcrypt.hash(password, salt);
 
     // Check if email already exists
     const existingUser = await User.findOne({ email });
@@ -34,6 +36,50 @@ const hashedPassword = await bcrypt.hash(password, salt)
     console.error(err);
     res.status(500).json({ message: "Internal Server Error" });
   }
+});
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Check if user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid Email" });
+    }
+
+    // Compare passwords
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid Password" });
+    }
+
+    const token = jwt.sign({ _id: user._id }, "mysecretKey");
+    // Return success response
+    // res.send(token);
+
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 1 week
+    });
+
+    res.json({ message: "Login successful" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+router.get("/user", async (req, res) => {
+  const cookie = req.cookies["jwt"];
+  const claims = jwt.verify(cookie, "mysecretKey");
+
+  if (!claims) {
+    return res.status(401).json({ message: "Unauthenticated" });
+  }
+
+  const user = await User.findOne({ _id: claims._id });
+  res.send(user);
 });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,23 +1,33 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+// const bodyParser = require("body-parser");
+const cors = require("cors");
+const cookieparser = require("cookie-parser");
+
 const app = express();
 
 // MongoDB Atlas connection
 const MONGODB_URI =
   "mongodb+srv://vishalrahangdale14:LSFCDuIbwxBK6u84@cluster0.7fdoxzo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 mongoose
-  .connect(MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(MONGODB_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
 const route = require("./Routes/routes");
+app.use(express.json());
+app.use(cookieparser());
+app.use(cors({
+  credentials: true,
+  origin: ['http://localhost:3000']
+}))
 
 app.use("/api", route);
 
+app.get("/", (req, res)=>{
+  res.send("Hello, world!");
+})
 
 const port = process.env.PORT || 5000;
 
